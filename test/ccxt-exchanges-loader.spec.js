0fdf0891f501ed798d533c788912899f31d75d5f
Kevin Purnelle <kevin.purnelle@gmail.com>
2018-08-16 11:06:58 +0200

    feat: loader will throw an error if an exchange does not exist

--- test/ccxt-exchanges-loader.spec.js
+++ test/ccxt-exchanges-loader.spec.js
@@ -1,21 +1,45 @@
 const fs = require('fs')
 const path = require('path')
 
-const filterExchanges = require('../lib/filterExchanges')
+const sourceModifier = require('../lib/sourceModifier')
+const expectedExchangesFromSource = require('./expectedExchangesFromSource')
 const ccxtSource = fs.readFileSync(path.join(__dirname, 'ccxtSourceMock.js'), 'utf8')
 const expectedSource = fs.readFileSync(path.join(__dirname, 'expectedSource.js'), 'utf8')
 
-describe('filterExchanges', () => {
-  it('should replace the require statement in ccxt source', () => {
-    const exchangesToKeep = [
-      'binance',
-      'kraken',
-      'bittrex',
-      'huobi',
-      'coinbase',
-    ]
+describe('sourceModifier', () => {
+  describe('parseExchangesFromSource()', () => {
+    it('should gather all the exchanges from ccxt source', () => {
+      const parsedExchanges = sourceModifier.parseExchangesFromSource(ccxtSource)
+      expect(parsedExchanges).toEqual(expectedExchangesFromSource)
+    })
+  })
 
-    const updatedSource = filterExchanges(ccxtSource, exchangesToKeep)
-    expect(updatedSource).toBe(expectedSource)
+  describe('filterExchanges', () => {
+    it('should replace the require statement in ccxt source', () => {
+      const exchangesToKeep = [
+        'binance',
+        'kraken',
+        'bittrex',
+        'huobi',
+        'coinbase',
+      ]
+
+      const updatedSource = sourceModifier.filterExchanges(ccxtSource, exchangesToKeep)
+      expect(updatedSource).toBe(expectedSource)
+    })
+
+    it('should throw an error if exchange does not exist in the source', () => {
+      const exchangesToKeep = [
+        'kraken',
+        'bynance', // <- BAD
+        'bittrex',
+        'huobi',
+        'coinbase',
+      ]
+
+      expect(() => {
+        sourceModifier.filterExchanges(ccxtSource, exchangesToKeep)
+      }).toThrowError('"bynance" exchange not found in ccxt source.')
+    })
   })
 })
